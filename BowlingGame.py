4ffaee07b79c0b48335ca2f02f2c6c6518811770
James Vanner <james.vanner@outlook.com>
2021-11-26 14:34:21 +1300

    Added docstrings

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,13 +1,17 @@
 
 class BowlingGame:
+    """Class for Bowling Game logic."""
 
     def __init__(self):
+        """Initialise list of rolls."""
         self.rolls=[]
 
     def roll(self,pins):
+        """Add roll score to rolls list."""
         self.rolls.append(pins)
 
     def score(self):
+        """Calculate and return game score."""
         result = 0
         rollIndex = 0
 
@@ -24,16 +28,21 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        """Check to see if strike has been scored."""
         return self.rolls[rollIndex] == 10
 
     def isSpare(self, rollIndex):
+        """Check to see if spare has been scored."""
         return self.rolls[rollIndex] + self.rolls[rollIndex+1] == 10
 
     def strikeScore(self,rollIndex):
+        """Return score for a strike and the following 2 rolls."""
         return  10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
 
     def spareScore(self,rollIndex):
+        """Return score for a spare and the following roll."""
         return  10 + self.rolls[rollIndex + 2]
 
     def frameScore(self, rollIndex):
+        """Return score for a normal frame."""
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
--- Main.py
+++ Main.py
@@ -3,19 +3,24 @@ import unittest
 import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
+    """Class for unit testing BowlingGame.py."""
 
     def setUp(self):
+        """Initiate game."""
         self.game = BowlingGame.BowlingGame()
 
     def testGutterGame(self):
+        """Test for all gutter balls."""
         self.rollMany(0, 20)
         assert self.game.score()==0
 
     def testAllOnes(self):
+        """Test for all ones."""
         self.rollMany(1, 20)
         assert self.game.score()==20
 
     def testOneSpare(self):
+        """Test for one spare."""
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -23,6 +28,7 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==16
 
     def testOneStrike(self):
+        """Test for one strike."""
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -30,14 +36,17 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==24
 
     def testPerfectGame(self):
+        """Test for a perfect game (all strikes)."""
         self.rollMany(10,12)
         assert self.game.score()==300
 
     def testAllSpares(self):
+        """Test for all spares."""
         self.rollMany(5,21)
         assert self.game.score()==150
 
     def rollMany(self, pins, rolls):
+        """Method for inputting several rolls of the same score."""
         for i in range(rolls):
             self.game.roll(pins)
 
